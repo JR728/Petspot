03f5807bf54e67568c64e9916f6f8f27a486b072
Vivid Quill <sjdoublec@gmail.com>
2023-09-24 16:48:49 -0500

    Changes to views/login.handlebars. Has a get to 'homepage' route that works.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,30 +1,14 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// Route to handle user registration
-router.post('/register', async (req, res) => {
-  try {
-    const userData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    // You can handle user sessions or other logic here if needed
-
-    res.status(200).json(userData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// Route to handle user login
+// Route to handle user login:
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
+      console.log('No user found.');
       res.status(400).json({ message: 'Incorrect email or password' });
       return;
     }
@@ -32,28 +16,53 @@ router.post('/login', async (req, res) => {
     const validPassword = userData.checkPassword(req.body.password);
 
     if (!validPassword) {
+      console.log('No password match.');
       res.status(400).json({ message: 'Incorrect email or password' });
       return;
     }
 
-    // Set up user session or authentication token here
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-    res.status(200).json({ message: 'Login successful' });
+      res.json({ user: userData, message: 'Login successful.' });
+    });
+
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
+// Route to handle user registration
+// router.post('/register', async (req, res) => {
+//   try {
+//     const userData = await User.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+
+// You can handle user sessions or other logic here if needed
+
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
 // Route to handle user logout (if needed)
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
+// router.post('/logout', (req, res) => {
+//   if (req.session.loggedIn) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-// const withAuth = require('../utils/auth'); Is this needed as part of router.get to homepage ('/')?
+// const withAuth = require('../utils/auth'); // Is this needed as part of router.get to homepage ('/')?
 
 router.get('/', async (req, res) => {
+    console.log(req.session);
   try {
+    // Get all Post data and JOIN with User 'username': 
     const dbPostData = await Post.findAll({
-      attributes: [
-        'id',
-        'title',
-        'created_at',
-        'post_content'
-      ],
       include: [
         {
           model: User,
@@ -20,15 +16,10 @@ router.get('/', async (req, res) => {
     });
     
     const posts = dbPostData.map(post => post.get({ plain: true }));
-
-    // Check if the user is not logged in, and if so, redirect to the login page:
-    if (!req.session.loggedIn) {
-      return res.redirect('/login');
-    }
-
+   
     res.render('homepage', {
       posts,
-      loggedIn: req.session.loggedIn
+      loggedIn: req.session.loggedIn //logged_in?
     });
     
   } catch (err) {
@@ -45,12 +36,48 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/signup', (req, res) => { 
-  // If the user goes to the '/signup' route but is already logged in, redirect to the homepage
-  if (req.session.loggedIn) {
-    return res.redirect('/');
-  }
-  res.render('signup'); // You can create a signup.handlebars for this route
-});
+// router.get('/signup', (req, res) => { 
+//   // If the user goes to the '/signup' route but is already logged in, redirect to the homepage
+//   if (req.session.loggedIn) {
+//     return res.redirect('/');
+//   }
+//   res.render('signup'); // You can create a signup.handlebars for this route
+// });
 
 module.exports = router;
+
+// router.get('/', async (req, res) => {
+//   console.log(req.session);
+// try {
+//   const dbPostData = await Post.findAll({
+//     attributes: [
+//       'id',
+//       'title',
+//       'created_at',
+//       'post_content'
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   });
+  
+//   const posts = dbPostData.map(post => post.get({ plain: true }));
+
+//   // Check if the user is not logged in, and if so, redirect to the login page:
+//   if (!req.session.loggedIn) {
+//     return res.redirect('/login');
+//   }
+
+//   res.render('homepage', {
+//     posts,
+//     loggedIn: req.session.loggedIn
+//   });
+  
+// } catch (err) {
+//   console.log(err);
+//   res.status(500).json(err);
+// }
+// });
