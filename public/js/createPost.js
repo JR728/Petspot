fa72e218d02095fdc1f242359b069f00a2368bed
JR728 <135184435+JR728@users.noreply.github.com>
2023-09-27 19:49:32 -0500

    added createPost.js

---
+++ public/js/createPost.js
@@ -0,0 +1,25 @@
+async function postFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const postContent = document.querySelector('#post-content').value.trim();
+
+  const response = await fetch('/api/posts/create', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      postContent
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', postFormHandler);
